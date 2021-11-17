f043f95c800cfe8566172860988512e52b5916c6
nmention <72927865+nmention@users.noreply.github.com>
2021-11-17 11:18:10 +0100

    Commentaires

--- probas.py
+++ probas.py
@@ -3,13 +3,14 @@ import random
 compteur = 0
 nbFaceDees = (int)(input("Donnez le nombre de faces de ce dé"))
 valeurDees = []
-event = 2
+event = 2 # évènement associé à la probabilité
 
-
+# initialisation du nb des faces du dées + initialisation des valeurs pour chaque face
 for faceDee in range(nbFaceDees):
     valeurDees.append((int)((input("Donnez la valeur de la face : " + (str)(faceDee)))))
 print(valeurDees)
 
+# tirage aléatoire pour 100 expériences
 for i in range(100):
     tirage = random.choice(valeurDees);
     print(tirage)
